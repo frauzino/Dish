9ea2ad85c0a8f652a48ff2607a1b8b109ca87588
Richard Young <richarddfyoung@gmail.com>
2023-10-17 19:18:47 -0400

    added rapidfire surveys gem and basic config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def can_administer?
+    current_user.try(:admin?)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  mount Rapidfire::Engine => "/rapidfire"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'pages#index'
 
-  get '/survey', to: 'pages#survey'
+  get 'survey', to: 'pages#survey'
   get 'leaderboard', to: 'pages#leaderboard'
   get 'project', to: 'pages#project'
   get 'account', to: 'pages#account'
