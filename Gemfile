dd04127a482fb19ec5c74092d1ad18b3d7170b1c
Richard Young <richarddfyoung@gmail.com>
2023-11-11 10:06:29 -0800

    visuals for devise views(login, signup, forgot password), added default value to users: points

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ gem "font-awesome-sass", "~> 6.1"
 gem "simple_form", github: "heartcombo/simple_form"
 gem "devise"
 gem 'rapidfire'
+gem 'faker'
 # gem 'jquery-rails'
 
 group :development, :test do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def can_administer?
     current_user.try(:is_admin?)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username first_name last_name gender])
   end
 end
