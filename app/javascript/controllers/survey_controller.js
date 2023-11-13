a9f6f898d9cde0bb75bb2578f9759a7c710c42a4
Richard Young <richarddfyoung@gmail.com>
2023-11-13 12:52:52 -0800

    javasript to display only active question, plus next button to advance question, plus css changes to buttons

--- app/javascript/controllers/survey_controller.js
+++ app/javascript/controllers/survey_controller.js
@@ -2,7 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="survey"
 export default class extends Controller {
-  connect() {
-    console.log('hello survey');
+
+  static targets = ["surveyElement"]
+
+  // connect() {
+  //   this.surveyElementTargets[0].classList.remove("hidden");
+  //   this.surveyElementTargets[0].classList.add("show");
+  //   console.log('test');
+  // }
+
+  next() {
+    for(let i = 0; i < this.surveyElementTargets.length; i++ ) {
+      if (event.target.id == `next${i}`) {
+        this.surveyElementTargets[i].classList.add("hidden");
+
+        this.surveyElementTargets[i+1].classList.remove("hidden");
+        break;
+      };
+    }
   }
 }
