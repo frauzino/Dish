40dac645478a446695fd3d4ffcc69d4721b62258
Richard Young <richarddfyoung@gmail.com>
2023-12-21 18:50:16 -0800

    fix spelling error in depenent

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,9 +1,9 @@
 class Survey < ApplicationRecord
-  has_many :survey_questions, dependant: :destroy
-  has_many :questions, through: :survey_questions, dependant: :destroy
+  has_many :survey_questions, dependent: :destroy
+  has_many :questions, through: :survey_questions, dependent: :destroy
   accepts_nested_attributes_for :survey_questions
   accepts_nested_attributes_for :questions
   belongs_to :user
-  has_one :result, dependant: :destroy
+  has_one :result, dependent: :destroy
   has_one_attached :photo
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   belongs_to :school
-  has_many :surveys, dependant: :destroy
-  has_many :user_badges, dependant: :destroy
-  has_many :badges, through: :user_badges, dependant: :destroy
+  has_many :surveys, dependent: :destroy
+  has_many :user_badges, dependent: :destroy
+  has_many :badges, through: :user_badges, dependent: :destroy
   has_one_attached :photo
-  has_one :referral, dependant: :destroy
+  has_one :referral, dependent: :destroy
 
   def survey_name
     "#{last_name}, #{first_name}"
