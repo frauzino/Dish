6706f9e6e2268a7e0138837f4ab986bb4c0846c4
Richard Young <richarddfyoung@gmail.com>
2023-12-21 18:43:38 -0800

    added dependant:destroy to all models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   belongs_to :school
-  has_many :surveys
-  has_many :user_badges
-  has_many :badges, through: :user_badges
+  has_many :surveys, dependant: :destroy
+  has_many :user_badges, dependant: :destroy
+  has_many :badges, through: :user_badges, dependant: :destroy
   has_one_attached :photo
-  has_one :referral
+  has_one :referral, dependant: : destroy
 
   def survey_name
     "#{last_name}, #{first_name}"
