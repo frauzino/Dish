a435b006ec4f6b5097edd9cc4fef2383199ed255
Richard Young <richarddfyoung@gmail.com>
2024-07-24 16:01:57 -0400

    setting up sendgrid for production email smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'letsdish.io' }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: 'letsdish.io',
+    user_name: 'apikey',
+    password: ENV['SENDGRID_CONTACT_FORM_API_KEY'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
